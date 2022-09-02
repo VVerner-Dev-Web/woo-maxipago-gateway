769db4cf73ff386a6f4024f255aebffac0670869
v-verner <vini.verner1@gmail.com>
2022-09-02 15:38:41 -0300

    initial files

---
+++ woo-maxipago-gateway.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Plugin Name:           Maxipago Gateway
+ * Description:           Integra o gateway de pagamento Maxipago ao WooCommerce
+ * Author:                VVerner
+ * Author URI:            https://vverner.com
+ * Version:               0.0.1
+ * License:               GPLv3 or later
+ * WC requires at least:  6.5
+ * WC tested up to:       6.8.2
+ * Requires at least:     5.6
+ * Tested up to:          6.0.2
+ * Requires PHP:          7.1
+ */
